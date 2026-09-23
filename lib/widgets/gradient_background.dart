0773d70dabfdda821e3e928a024bc4e4978ff4c4
Abidalfakhri <abidfakhri5@gmail.com>
2026-09-23 15:37:58 +0700

    feat: initialize AnimeVerse project

---
+++ lib/widgets/gradient_background.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class GradientBackground extends StatelessWidget {
+  const GradientBackground({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF333465),
+            Color(0xFF0B395E),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: child,
+    );
+  }
+}
